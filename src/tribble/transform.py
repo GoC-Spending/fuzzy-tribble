5800e30b65c29a0e2c829d225707a59e79153da6
Jason White <jason.white@shopify.com>
2017-09-19 23:54:59 -0400

    load pandas dataframe from dir of json files

---
+++ src/tribble/transform.py
@@ -0,0 +1,34 @@
+import itertools
+import json
+import os
+import typing
+import pandas
+
+
+def transform_dir(input_dir: str, grouping_length: int = 100) -> pandas.DataFrame:
+    df: typing.Optional[pandas.DataFrame] = None
+
+    blobs = _json_blobs(input_dir)
+    for group in _grouper(blobs, grouping_length):
+        chunk = [x for x in group if x]
+        df = pandas.DataFrame(chunk) if df is None else df.append(chunk)
+
+    return df or pandas.DataFrame()
+
+
+T = typing.TypeVar('T')
+
+
+def _grouper(iterable: typing.Iterable[T], n: int) -> typing.Iterable[T]:
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=None)
+
+
+def _json_blobs(input_dir: str) -> typing.Iterator[typing.Dict[str, typing.Any]]:
+    input_filenames = [os.path.join(input_dir, dir_name) for dir_name in os.listdir(input_dir)]
+
+    for filename in input_filenames:
+        with open(filename) as input_file:
+            yield json.loads(input_file.read())
---
+++ test/test_transform.py
@@ -0,0 +1,73 @@
+import json
+import py._path.local
+import typing
+from tribble import transform
+
+
+def data_template(overrides: typing.Dict[str, typing.Any]) -> typing.Dict[str, typing.Any]:
+    data = {
+        "uuid": "tbs-0000000000",
+        "vendorName": "ABC Company",
+        "referenceNumber": "0000000000",
+        "contractDate": "2012-03-31",
+        "description": "499 OTHER PROFESSIONAL SERVICES NOT ELSEWHERE SPECIFIED",
+        "extraDescription": "Big Contract #1",
+        "objectCode": "0499",
+        "contractPeriodStart": "2012-04-01",
+        "contractPeriodEnd": "2018-03-31",
+        "startYear": "2012",
+        "endYear": "2018",
+        "deliveryDate": "",
+        "originalValue": 6000.0,
+        "contractValue": 6000,
+        "comments": "This contract includes one or more amendments.This contract was competitively sourced.This contract is a multi-year contract.",
+        "ownerAcronym": "tbs",
+        "sourceYear": 2012,
+        "sourceQuarter": 1,
+        "sourceFiscal": "201213-Q4",
+        "sourceFilename": "tbs\/5ae78038dd512ae3f7e8a91349f443cb.html",
+        "sourceURL": "http:\/\/www.tbs-sct.gc.ca\/scripts\/contracts-contrats\/reports-rapports-eng.asp?r=c&refNum=0000000000&q=4&yr=2012&d=",
+        "amendedValues": [],
+        "contractPeriodRange": "2012-04-01 to 2018-03-31",
+        "yearsDuration": 6,
+        "valuePerYear": 1000.0,
+        "vendorClean": "Big Contract #1"
+    }
+    data.update(overrides)
+    return data
+
+
+def test_transform_dir(tmpdir: py._path.local.LocalPath) -> None:
+    data1_file = tmpdir.join('data1.json')
+    data1 = data_template({
+        "uuid": "tbs-0000000000",
+        "vendorName": "ABC Company",
+        "referenceNumber": "0000000000",
+    })
+    data1_file.write(json.dumps(data1))
+
+    data2_file = tmpdir.join('data2.json')
+    data2 = {
+        "uuid": "tbs-0000000001",
+        "vendorName": "XYZ Company",
+        "referenceNumber": "0000000001",
+    }
+    data2_file.write(json.dumps(data2))
+
+    output = transform.transform_dir(str(tmpdir))
+    assert len(output) == 2
+
+
+def test_chunking(tmpdir: py._path.local.LocalPath) -> None:
+    for i in range(0, 100):
+        data = data_template({'uuid': 'tbs-{}'.format(i)})
+        data_file = tmpdir.join('data{}.json'.format(i))
+        data_file.write(json.dumps(data))
+
+    output = transform.transform_dir(str(tmpdir), grouping_length=7)
+    assert len(output) == 100
+
+
+def test_blank_dir(tmpdir: py._path.local.LocalPath) -> None:
+    output = transform.transform_dir(str(tmpdir))
+    assert len(output) == 0
