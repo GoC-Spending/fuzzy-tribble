86bfe11ad3ce14bfa789dec4d1d2348f48eb9958
Jason White <jason.white@shopify.com>
2017-10-01 17:10:35 -0400

    adding basic test db

---
+++ script/build_container.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+
+apt-get update
+apt-get install -y libprotobuf-dev protobuf-compiler software-properties-common python-software-properties python3-pip
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update && apt-get install -y python3.6 python3.6-dev
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y mysql-server-5.7
+mkdir -p /var/lib/mysql
+mkdir -p /var/run/mysqld
+mkdir -p /var/log/mysql
+
+sed -i -e "$ a [client]\n\n[mysql]\n\n[mysqld]"  /etc/mysql/my.cnf
+sed -i -e "s/\(\[client\]\)/\1\ndefault-character-set = utf8/g" /etc/mysql/my.cnf
+sed -i -e "s/\(\[mysql\]\)/\1\ndefault-character-set = utf8/g" /etc/mysql/my.cnf
+sed -i -e "s/\(\[mysqld\]\)/\1\ninit_connect='SET NAMES utf8'\ncharacter-set-server = utf8\ncollation-server=utf8_unicode_ci\nbind-address = 0.0.0.0/g" /etc/mysql/my.cnf
+
+apt-get clean
+
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+ln -sf /usr/bin/python3 /usr/bin/python && ln -s /usr/bin/pip3 /usr/bin/pip
+pip install --upgrade pip
---
+++ script/run_tests.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld /var/log/mysql
+service mysql start
+
+pytest tests
