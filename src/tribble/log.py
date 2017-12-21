9b30e18bed621a7c03c1fcae7ae74ccec02230bb
Jonathan Enders <jonathan.enders@gmx.de>
2017-12-21 12:27:58 -0500

    fixed typing, fixed logging error

--- src/tribble/log.py
+++ src/tribble/log.py
@@ -1,8 +1,7 @@
 import logging
-from io import IOBase
 import sys
 import types
-from typing import Union
+from typing import Union, Any, Callable
 from tqdm import tqdm
 
 logging.basicConfig(level=logging.INFO,
@@ -22,12 +21,12 @@ LOGGER = get_logger(__name__)
 class TqdmLog(tqdm):
 
     class PreserveLastOutputStreamInterceptor():
-        def __init__(self, output_stream: IOBase, last_output_initial_value: str = "(no output yet)"):
+        def __init__(self, output_stream: Any, last_output_initial_value: str = "(no output yet)") -> None:
             self.output_stream = output_stream
             if hasattr(output_stream, 'encoding'):
                 self.encoding = output_stream.encoding
             self.last_output = last_output_initial_value
-        def write(self, string: str):
+        def write(self, string: str) -> None:
             if string != '\n' and string != '':   # don't store or print the end of line after the last iteration
                 self.last_output = string
                 self.output_stream.write(string)
@@ -35,11 +34,11 @@ class TqdmLog(tqdm):
                 # get rid of the status bar line so that it is
                 # effectively replaced by the log of the last output
                 self.output_stream.write('\r')
-        def flush(self):
+        def flush(self) -> None:
             self.output_stream.flush()
 
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         if 'file' not in kwargs:
             kwargs['file'] = TqdmLog.PreserveLastOutputStreamInterceptor(sys.stderr)
         super(tqdm, self).__init__(*args, **kwargs)
@@ -52,7 +51,7 @@ class TqdmLog(tqdm):
                 for count, item in enumerate(iterable):
                     yield item
             finally:
-                if self.n != count:
+                if self.n != count + 1:
                     print('')  # kinda dirty but ensures that the next log line is not appended to the last output line
                     LOGGER.info(f'failed on item: {item}')
                     self.n = count
@@ -60,8 +59,8 @@ class TqdmLog(tqdm):
                 LOGGER.info(self.fp.last_output[1:])
         return iter_logging_wrapper()
     @classmethod
-    def pandas(tclass: type, *targs, **tkwargs): # pylint: disable=C0202
-                                                 # adhering to tqdm's definition
+    def pandas(tclass: type, *targs, **tkwargs) -> None: # pylint: disable=C0202
+                                                         # adhering to tqdm's definition
         if 'file' in tkwargs:
             super(tqdm, tclass).pandas(*targs, **tkwargs)
         else:
@@ -76,10 +75,10 @@ class TqdmLog(tqdm):
             from pandas import Panel
 
             tqdm.pandas(tclass, *targs, **tkwargs)
-            def wrap_progress_apply(original_progress_apply) -> types.FunctionType:
+            def wrap_progress_apply(original_progress_apply) -> Callable[[Any, Callable, Any, Any], Any]:
                 def progress_apply(df: Union[DataFrame, Series, DataFrameGroupBy,
                                              SeriesGroupBy, GroupBy, PanelGroupBy, Panel],
-                                   func: types.FunctionType, *args, **kwargs
+                                   func: Callable, *args, **kwargs
                                   ) -> Union[DataFrame, Series, DataFrameGroupBy,
                                              SeriesGroupBy, GroupBy, PanelGroupBy, Panel]:
                     # error handling in progress_apply is difficult because
