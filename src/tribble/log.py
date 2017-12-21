61ca897aa539cf192f12ca80184208221bbb87ea
Jonathan Enders <jonathan.enders@gmx.de>
2017-12-21 13:04:32 -0500

    pleased pylint

--- src/tribble/log.py
+++ src/tribble/log.py
@@ -98,7 +98,8 @@ class TqdmLog(tqdm):
                             res = func(item, *args, **kwargs)
                         except Exception: # pylint: disable=W0703
                                           # needs to be general
-                            print('')  # kinda dirty but ensures that the next log line is not appended to the last output line
+                            print('')  # kinda dirty but ensures that the next log line 
+                                       # is not appended to the last output line
                             LOGGER.info(f'Failed on pandas apply during the {count}. invocation of the ' +
                                         f'provided apply function processing item: \n{item}')
                         count+=1
